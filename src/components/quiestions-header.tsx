b442f94bd500dd29864d8299dd132f3408efa675
kamil <kamilmaslanka212@gmail.com>
2025-02-28 18:50:30 +0100

    show correct answers button

--- src/components/quiestions-header.tsx
+++ src/components/quiestions-header.tsx
@@ -1,10 +1,15 @@
 import { FaListOl } from "react-icons/fa";
+import { Button } from "./ui/button";
 
 export default function QuestionsHeader() {
   return (
-    <div className="flex items-center gap-2 mb-4">
-      <FaListOl className="text-2xl text-indigo-600" />
-      <h2 className="text-2xl font-bold text-gray-800">Questions</h2>
+    <div className="flex items-center justify-between  mb-4">
+      <div className="flex items-center gap-2">
+        <FaListOl className="text-2xl text-indigo-600" />
+
+        <h2 className="text-2xl font-bold text-gray-800">Questions</h2>
+      </div>
+      <Button variant={"secondary"}>Show correct answers</Button>
     </div>
   );
 }
